4b3066ceed326d42c5cefe540afbe385d0c6322f
jkingstonk <justin.kingston.414@my.csun.edu>
2020-10-19 17:31:13 -0700

    Added a TurnTracker class.

--- TurnTracker.java
+++ TurnTracker.java
@@ -1,6 +1,7 @@
 public class TurnTracker
 {
     private int turnCount;
+    //If turnCount == -1, the game has not been started yet.
     //If turnCount == 0, player is choosing ships.
     //If turnCount > 0 && % 2 = 1, it is the player's turn.
     //If turnCount > 0 && % 2 = 0, it is the opponent's turn.
