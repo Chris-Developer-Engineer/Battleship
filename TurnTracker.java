16ef83d46267add7e91514cc894a83e31b3d7474
jkingstonk <justin.kingston.414@my.csun.edu>
2020-10-19 17:30:06 -0700

    Added a TurnTracker class.

---
+++ TurnTracker.java
@@ -0,0 +1,25 @@
+public class TurnTracker
+{
+    private int turnCount;
+    //If turnCount == 0, player is choosing ships.
+    //If turnCount > 0 && % 2 = 1, it is the player's turn.
+    //If turnCount > 0 && % 2 = 0, it is the opponent's turn.
+
+    //TurnTracker constructor
+    public TurnTracker()
+    {
+        this.turnCount = -1;
+    }
+
+    //Increment the turnCount
+    public void incrementTurn()
+    {
+        turnCount += 1;
+    }
+
+    //Return the turnCount
+    public int getTurnCount()
+    {
+        return turnCount;
+    }
+}
