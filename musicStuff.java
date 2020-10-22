2fdcbceaa3d97517d652c34c57eb12359d849800
Christopher Chen <yuntian.chen.485@my.csun.edu>
2020-10-22 02:09:55 +0000

    Update musicStuff.java

--- musicStuff.java
+++ musicStuff.java
@@ -1,44 +1,41 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
-import java.awt.Component;
-import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import java.io.File;
 
 public class musicStuff {
-    public musicStuff() {
-    }
-
-    void playMusic(String musicLocation) {
-        boolean status = false;
-
-        try {
+    void playMusic(String musicLocation){
+        int status = 0;
+        try{
             File musicPath = new File(musicLocation);
-            if (musicPath.exists()) {
+
+            if(musicPath.exists()){
                 AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
                 Clip clip = AudioSystem.getClip();
                 clip.open(audioInput);
                 clip.start();
-                clip.loop(-1);
-                status = true;
-                JOptionPane.showMessageDialog((Component)null, "press ok to pause playing");
-                long clipTimePostion = clip.getMicrosecondPosition();
-                clip.stop();
-                JOptionPane.showMessageDialog((Component)null, "press ok to Resume");
-                clip.setMicrosecondPosition(clipTimePostion);
-                clip.start();
-                JOptionPane.showMessageDialog((Component)null, "press ok to stop playing");
-            } else {
+                clip.loop(clip.LOOP_CONTINUOUSLY);
+                status = 1;
+
+                while(status ==1) {
+                    JOptionPane.showMessageDialog(null, "press ok to pause playing");
+                    long clipTimePostion = clip.getMicrosecondPosition();
+                    clip.stop();
+
+                    JOptionPane.showMessageDialog(null, "press ok to Resume");
+                    clip.setMicrosecondPosition(clipTimePostion);
+                    clip.start();
+                }
+            }
+
+            else{
                 System.out.println("Can't find file");
             }
-        } catch (Exception var8) {
-            var8.printStackTrace();
         }
-
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
     }
 }
+
