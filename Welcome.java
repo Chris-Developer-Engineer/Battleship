bfa494ab4eb6ed8a590f5d04b86faa633d7670f9
Christopher Chen <yuntian.chen.485@my.csun.edu>
2020-10-22 01:52:04 +0000

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -28,6 +28,9 @@ public class Welcome {
     public Welcome(){
         setScreen();
         mainScreen.setVisible(true);
+        String filePath = "C:\\Users\\yuntian chen\\IdeaProjects\\Ardent-Christopher Chen\\Music\\Heroic Feat (Main).wav";
+        musicStuff musicObject = new musicStuff();
+        musicObject.playMusic((filePath));
     }
 
     public void setScreen() {
