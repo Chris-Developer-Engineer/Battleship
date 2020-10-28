e14c53e7cc31174184d849972c1938dc7a614e1e
Brandon <brandon.wion.161@my.csun.edu>
2020-10-27 20:22:59 -0700

    Win Loss Class Added

---
+++ Settings.java
@@ -0,0 +1,34 @@
+import javax.swing.*; //GUI
+import java.awt.*; //Font,Dimensions
+import java.awt.event.*; //Listeners
+import javax.swing.border.*;
+
+public class Settings {
+    JFrame settingsScreen = new JFrame("Settings");
+
+    //Button
+    JButton save = new JButton("Save");
+
+    public Settings(){
+        setScreen();
+        settingsScreen.setVisible(true);
+    }
+
+    public void setScreen(){
+        settingsScreen.setLocation(490, 190);
+        settingsScreen.setLayout(new FlowLayout(FlowLayout.CENTER));
+        settingsScreen.setSize(300,400);
+
+        settingsScreen.add(save, BorderLayout.PAGE_END);
+
+
+        //Action Listener
+        save.addActionListener( //Main Menu button
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        settingsScreen.dispose();
+                    }
+                }
+        );
+    }
+}
