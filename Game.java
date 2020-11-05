48d29e5f328105fd85c7ca82313dce290ed35641
ejoreillyiv22 <ejoreillyiv22@protonmail.com>
2020-11-04 18:09:19 -0800

    Updated to work with Chris's files.

--- Game.java
+++ Game.java
@@ -1,31 +1,48 @@
 import javax.swing.*; //GUI
+import javax.swing.border.*;
 import java.awt.*; //Font
 import java.awt.event.*; //Listeners
+import java.awt.Color; //Color
+import java.awt.event.*; //Popup
+
 
 public class Game {
-    private final int WIDTH = 500, LENGTH = 500;
+    private final int WIDTH = 1250, LENGTH = 700;
     private Style style = new Style();
-    private JFrame gameScreen = style.frame("Welcome", WIDTH, LENGTH);
-    private TurnTracker tracker = new TurnTracker();
+    private JFrame gameScreen = style.frame("Game Play", WIDTH, LENGTH);
+    private ImageIcon background;
+    private JLabel back;
 
     //Menu Instantiation
     private JMenuBar mainBar = new JMenuBar();
     private JMenu fileMenu = style.menu("File");
     private JMenuItem exitItem = style.menuItem("Exit");
+    final JPopupMenu popup = new JPopupMenu();
 
-    //Button/Labels Instantiation
-    private JButton board1 = style.button("Play with a 7x7 board");
-    private JButton board2 = style.button("Play with a 8x8 board");
-    private JButton board3 = style.button("Play with a 9x9 board");
+    //Images Instantiation....to update image replace file name
+    Icon mainMenuImage = new ImageIcon(getClass().getResource("menu.png"));
+    Icon settingsImage = new ImageIcon(getClass().getResource("settingsGame.png"));
+
+    //Buttons
+    private JButton mainMenu = new JButton(mainMenuImage);
+    private JButton settings = new JButton(settingsImage);
+
+    //Labels
+    private JLabel winLossLabel = new JLabel("Win/Loss Statistics");
+    private JLabel wins = new JLabel("Wins: " + WinLoss.win);
+    private JLabel loss = new JLabel("Losses: " + WinLoss.loss);
+    private JLabel rank = new JLabel(WinLoss.ranking());
 
     //Panel
-    private JPanel gamePanel = new JPanel(new GridBagLayout());
-    private JPanel board1Panel = new JPanel(new GridBagLayout());
-    private JPanel board2Panel = new JPanel(new GridBagLayout());
-    private JPanel board3Panel = new JPanel(new GridBagLayout());
+    private JPanel centerPanel = new JPanel(); //Game boards will be placed here
+    private JPanel winLossPanel = new JPanel(); //Game boards will be placed here
+    private JPanel shipHoldingPanel = new JPanel(); //Holds ships on bottom left of window
+    private JPanel opponentShips = new JPanel(); //Opponent's ship tracking panel
+    private JPanel playerShips = new JPanel(); //Player's ship tracking panel
+    private JPanel turnTracker = new JPanel(); //Displays turn tracking
 
-    //Box Layout
-    private BoxLayout layout = new BoxLayout(gamePanel, BoxLayout.Y_AXIS);
+    //Borders
+    Border raisedBorder = new EtchedBorder(EtchedBorder.RAISED); //Raised Border
 
     public Game() {
         setScreen();
@@ -33,24 +50,85 @@ public class Game {
     }
 
     public void setScreen(){
+        //Background set
+        background = new ImageIcon(getClass().getResource("water.jpg"));
+        back = new JLabel(background);
+        back.setSize(1250,700);
 
-        gameScreen.add(board1Panel);
-        gameScreen.add(board2Panel);
-        gameScreen.add(board3Panel);
-
-        board1Panel.add(board1);
-        board2Panel.add(board2);
-        board3Panel.add(board3);
-
+        gameScreen.add(back);
         gameScreen.setLocationRelativeTo(null);
-        gameScreen.setLayout(new FlowLayout());
+        gameScreen.getContentPane().setBackground(Color.black);
+        gameScreen.setLayout(new FlowLayout(FlowLayout.CENTER));
 
+        //Menu Instantiation
         mainBar.add(fileMenu);
         fileMenu.add(exitItem);
 
+        //Button Instantiation....if image is updated adjust bounds
+        mainMenu.setBounds(70,10,170,80);
+        settings.setBounds(70,100,170,80);
+
+        //Win Loss Statistics
+        winLossPanel.setBorder(raisedBorder);
+        winLossPanel.setLayout(new BoxLayout(winLossPanel, BoxLayout.Y_AXIS));
+        winLossPanel.setBackground(new Color(0,0,0,125)); //a is the transparency value
+        winLossPanel.setBounds(10,190,305,100);
+
+        winLossLabel.setFont(style.headlineFont());
+        winLossLabel.setForeground(Color.WHITE);
+
+        wins.setFont(style.bodyFont());
+        wins.setForeground(Color.WHITE);
+
+        loss.setFont(style.bodyFont());
+        loss.setForeground((Color.WHITE));
+
+        rank.setFont(style.bodyFont());
+        rank.setForeground(Color.WHITE);
+
+        winLossPanel.add(winLossLabel);
+        winLossPanel.add(wins);
+        winLossPanel.add(loss);
+        winLossPanel.add(rank);
+
+        //Ship Holding Panel (bottom left of screen)
+        shipHoldingPanel.setBorder(raisedBorder);
+        shipHoldingPanel.setBackground(new Color(0,0,0,125)); //a is the transparency value
+        shipHoldingPanel.setBounds(10,300,305,320);
+
+        //Set Center Panel (Game boards)
+        centerPanel.setBorder(raisedBorder);
+        centerPanel.setBackground(new Color(0,0,0, 125)); //a is the transparency value
+        centerPanel.setBounds(325,10,600,610);
+
+        //Player's ship tracking panel (middle right)
+        playerShips.setBorder(raisedBorder);
+        playerShips.setBackground(new Color(0,0,0,125)); //a is the transparency value
+        playerShips.setBounds(935,235,305,155);
+
+        //Turn Tracking Panel (bottom right)
+        turnTracker.setBorder(raisedBorder);
+        turnTracker.setBackground(new Color(0,0,0,125)); //a is the transparency value
+        turnTracker.setBounds(935,400,305,220);
+
+        //Opponent's ship tracking panel (upper right)
+        opponentShips.setBorder(raisedBorder);
+        opponentShips.setBackground(new Color(0,0,0,125)); //a is the transparency value
+        opponentShips.setBounds(935,70,305,155);
+
+        //Set Screen
+        back.add(centerPanel); //Panel for player vs AI game boards
+        back.add(turnTracker); //Panel to display turn history
+        back.add(shipHoldingPanel); //Panel to show available ships to place
+        back.add(opponentShips); //Panel to show opponents ships
+        back.add(playerShips); //Panel to show players ships
+        back.add(winLossPanel); //Panel to display win loss statistics
+        back.add(mainMenu);
+        back.add(settings);
         gameScreen.setJMenuBar(mainBar);
 
-        exitItem.addActionListener(
+        //Action Listeners
+        exitItem.addActionListener( //Exit button
                 new ActionListener(){
                     public void actionPerformed(ActionEvent e){
                         System.exit(0);
@@ -58,36 +136,19 @@ public class Game {
                 }
         );
 
-        board1.addActionListener(
+        mainMenu.addActionListener( //Main from menu bar
                 new ActionListener() {
                     public void actionPerformed(ActionEvent e) {
+                        new Welcome();
                         gameScreen.dispose();
-                        GameBoard game = new GameBoard(7); //Creates a 7x7 gameboard object
-                        game.initialize(); // Initializes the game
                     }
                 }
         );
 
-        board2.addActionListener(
-                new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        gameScreen.dispose();
-                        GameBoard game = new GameBoard(8); //Creates a 8x8 gameboard object
-                        game.initialize(); // Initializes the game
-                    }
-                }
-        );
-
-        board3.addActionListener(
-                new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        gameScreen.dispose();
-                        //edit this constructor for 9x9
-                        GameBoard game = new GameBoard(9); //Creates a 9x9 gameboard object
-                        game.initialize(); // Initializes the game
-                    }
-                }
-        );
-
+        settings.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                new Settings();
+            }
+        });
     }
 }
